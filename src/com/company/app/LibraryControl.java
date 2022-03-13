8786fa8f07dca23d0f3c501aa0972bc5e5d1042b
Sebastian Lech <lechsebastian99@gmail.com>
2022-03-13 11:58:39 +0100

    v1.0

--- src/com/company/app/LibraryControl.java
+++ src/com/company/app/LibraryControl.java
@@ -5,9 +5,9 @@ import com.company.model.Book;
 import com.company.model.Library;
 
 class LibraryControl {
-    private final int exit = 0;
-    private final int addBook = 1;
-    private final int printBooks = 2;
+    private static final int EXIT = 0;
+    private static final int ADD_BOOK = 1;
+    private static final int PRINT_BOOKS = 2;
     private DataReader dataReader = new DataReader();
     private Library library = new Library();
 
@@ -19,12 +19,12 @@ class LibraryControl {
             option = dataReader.getInt();
 
             switch (option) {
-                case addBook -> addBook();
-                case printBooks -> printBooks();
-                case exit -> exit();
+                case ADD_BOOK -> addBook();
+                case PRINT_BOOKS -> printBooks();
+                case EXIT -> exit();
                 default -> System.out.println("Nie ma takiej opcji! Sprobuj ponownie!");
             }
-        }while(option != exit);
+        }while(option != EXIT);
     }
 
     private void exit() {
@@ -43,9 +43,9 @@ class LibraryControl {
 
     private void printOptions() {
         System.out.println();
-        System.out.println(exit + " - wyjscie z programu");
-        System.out.println(addBook + " - dodanie nowej ksiazki");
-        System.out.println(printBooks + " - wyswietl dostepne ksiazki");
+        System.out.println(EXIT + " - wyjscie z programu");
+        System.out.println(ADD_BOOK + " - dodanie nowej ksiazki");
+        System.out.println(PRINT_BOOKS + " - wyswietl dostepne ksiazki");
         System.out.print("Wybierz opcję: ");
     }
 }
