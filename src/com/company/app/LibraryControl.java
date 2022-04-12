62cd8b02b5fc1922d25f8cf08f28552e5f59c8c7
Sebastian Lech <lechsebastian99@gmail.com>
2022-04-12 20:29:44 +0200

    v1.31

--- src/com/company/app/LibraryControl.java
+++ src/com/company/app/LibraryControl.java
@@ -6,21 +6,16 @@ import com.company.model.Library;
 import com.company.model.Magazine;
 
 class LibraryControl {
-    private static final int EXIT = 0;
-    private static final int ADD_BOOK = 1;
-    private static final int ADD_MAGAZINE = 2;
-    private static final int PRINT_BOOKS = 3;
-    private static final int PRINT_MAGAZINES = 4;
 
     private DataReader dataReader = new DataReader();
     private Library library = new Library();
 
     public void controlLoop(){
-        int option;
+        Option option;
 
         do{
             printOptions();
-            option = dataReader.getInt();
+            option = Option.createFromInt(dataReader.getInt());
 
             switch (option) {
                 case ADD_BOOK -> addBook();
@@ -30,7 +25,7 @@ class LibraryControl {
                 case EXIT -> exit();
                 default -> System.out.println("Nie ma takiej opcji! Sprobuj ponownie!");
             }
-        }while(option != EXIT);
+        }while(option != Option.EXIT);
     }
 
     private void exit() {
@@ -58,11 +53,9 @@ class LibraryControl {
 
     private void printOptions() {
         System.out.println();
-        System.out.println(EXIT + " - wyjscie z programu");
-        System.out.println(ADD_BOOK + " - dodanie nowej ksiazki");
-        System.out.println(ADD_MAGAZINE + " - dodanie nowego magazynu");
-        System.out.println(PRINT_BOOKS + " - wyswietl dostepne ksiazki");
-        System.out.println(PRINT_MAGAZINES + " - wyswietl dostepne magazyny");
+        for (Option value : Option.values()) {
+            System.out.println(value);
+        }
         System.out.print("Wybierz opcję: ");
     }
 }
---
+++ src/com/company/app/Option.java
@@ -0,0 +1,34 @@
+package com.company.app;
+
+public enum Option {
+    EXIT(0, " - wyjscie z programu"),
+    ADD_BOOK(1, " - dodanie nowej ksiazki"),
+    ADD_MAGAZINE(2, " - dodanie nowego magazynu"),
+    PRINT_BOOKS(3, " - wyswietl dostepne ksiazki"),
+    PRINT_MAGAZINES(4, " - wyswietl dostepne magazyny");
+
+    private final int value;
+    private final String description;
+
+    Option(int value, String description) {
+        this.value = value;
+        this.description = description;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    @Override
+    public String toString() {
+        return value + description;
+    }
+
+    static Option createFromInt(int option) {
+        return Option.values()[option]; //Zamiast swtich mozna uzyc takiego zapisu, bo opcje sa numerowane jak tablica
+    }
+}
